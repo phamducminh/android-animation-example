16df01b03d37443a0a1188f59bb21ab827415795
Minh Pham <minhpham@Minhs-MacBook-Pro-2.local>
2019-07-02 23:16:32 +0700

    Add basic animation resource files

--- app/src/main/java/com/minhpd/android_animation_example/MainActivity.java
+++ app/src/main/java/com/minhpd/android_animation_example/MainActivity.java
@@ -1,13 +1,61 @@
 package com.minhpd.android_animation_example;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements Animation.AnimationListener {
+
+    TextView txtMessage;
+    Button btn;
+    Animation anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtMessage = findViewById(R.id.text);
+        btn = findViewById(R.id.btn);
+
+        //loading Animation
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_blink);
+//        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_bounce);
+//        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_clockwise_rotation);
+//        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_fade_in);
+//        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_fade_out);
+//        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_translation_x);
+//        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_zoom_in);
+//        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_zoom_out);
+
+        //handling animation on button click
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtMessage.setVisibility(View.VISIBLE);
+                // starting the animation
+                txtMessage.startAnimation(anim);
+            }
+        });
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
